e486ed3961f10a4deff75a02ef758757358b5bf0
Beau <cummingsbeau@gmail.com>
2019-03-01 09:48:00 -0700

    added clock

--- models/clock.js
+++ models/clock.js
@@ -1,19 +1,58 @@
+module.exports = function (sequelize, DataTypes) {
+	var Post = sequelize.define("Post", {
+		dreamer_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		dream_description: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		mood: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		food_before_sleep: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		hours_of_sleep: {
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		tv_on: {
+			type: DataTypes.BOOLEAN,
+			default: false
+		}
+	});
+	Post.associate = function (models) {
+		// We're saying that a Post should belong to an dreamer
+		// A Post can't be created without an dreamer due to the foreign key constraint
+		Post.belongsTo(models.dreamer, {
+			foreignKey: {
+				allowNull: false
+			}
+		});
+	};
+	return Post;
+};
+
 var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav");
-		sound.loop = true;
+sound.loop = true;
 
 var h2 = document.getElementById('clock');
 
 // display current time by the second
-var currentTime = setInterval(function(){
+var currentTime = setInterval(function () {
 	var date = new Date();
-	
+
 	var hours = (12 - (date.getHours()));
 	// var hours = date.getHours();
-	
+
 	var minutes = date.getMinutes();
-	
+
 	var seconds = date.getSeconds();
-	
+
 	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
 
 
@@ -27,10 +66,10 @@ var currentTime = setInterval(function(){
 		hours = hours;
 	}
 
-	
+
 	h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
-	
-},1000);
+
+}, 1000);
 
 
 /*functions to get hour, min, secs, 
@@ -39,39 +78,39 @@ var currentTime = setInterval(function(){
 
 function addZero(time) {
 
-		return (time < 10) ? "0" + time : time;
-	
+	return (time < 10) ? "0" + time : time;
+
 }
 
-function hoursMenu(){
+function hoursMenu() {
 
 	var select = document.getElementById('alarmhrs');
 	var hrs = 12
 
-	for (i=1; i <= hrs; i++) {
-		select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
-		
+	for (i = 1; i <= hrs; i++) {
+		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+
 	}
 }
 hoursMenu();
 
-function minMenu(){
+function minMenu() {
 
 	var select = document.getElementById('alarmmins');
 	var min = 59;
 
-	for (i=0; i <= min; i++) {
+	for (i = 0; i <= min; i++) {
 		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
 	}
 }
 minMenu();
 
-function secMenu(){
+function secMenu() {
 
 	var select = document.getElementById('alarmsecs');
 	var sec = 59;
 
-	for (i=0; i <= sec; i++) {
+	for (i = 0; i <= sec; i++) {
 		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
 	}
 }
@@ -81,67 +120,67 @@ secMenu();
 function alarmSet() {
 
 	var hr = document.getElementById('alarmhrs');
-	
+
 	var min = document.getElementById('alarmmins');
-	
+
 	var sec = document.getElementById('alarmsecs');
-	
+
 	var ap = document.getElementById('ampm');
-    
 
-    var selectedHour = hr.options[hr.selectedIndex].value;
-    var selectedMin = min.options[min.selectedIndex].value;
-    var selectedSec = sec.options[sec.selectedIndex].value;
-    var selectedAP = ap.options[ap.selectedIndex].value;
 
-    var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
-    console.log('alarmTime:' + alarmTime);
+	var selectedHour = hr.options[hr.selectedIndex].value;
+	var selectedMin = min.options[min.selectedIndex].value;
+	var selectedSec = sec.options[sec.selectedIndex].value;
+	var selectedAP = ap.options[ap.selectedIndex].value;
 
-    document.getElementById('alarmhrs').disabled = true;
+	var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
+	console.log('alarmTime:' + alarmTime);
+
+	document.getElementById('alarmhrs').disabled = true;
 	document.getElementById('alarmmins').disabled = true;
 	document.getElementById('alarmsecs').disabled = true;
 	document.getElementById('ampm').disabled = true;
 
 
-//when alarmtime is equal to currenttime then play a sound
+	//when alarmtime is equal to currenttime then play a sound
 	var h2 = document.getElementById('clock');
 
-/*function to calcutate the current time 
-then compare it to the alarmtime and play a sound when they are equal
-*/
+	/*function to calcutate the current time 
+	then compare it to the alarmtime and play a sound when they are equal
+	*/
 
-setInterval(function(){
+	setInterval(function () {
 
-	var date = new Date();
-	
-	var hours = (12 - (date.getHours()));
-	// var hours = date.getHours();
-	
-	var minutes = date.getMinutes();
-	
-	var seconds = date.getSeconds();
-	
-	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+		var date = new Date();
+
+		var hours = (12 - (date.getHours()));
+		// var hours = date.getHours();
+
+		var minutes = date.getMinutes();
+
+		var seconds = date.getSeconds();
+
+		var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
 
 
-	//convert military time to standard time
+		//convert military time to standard time
 
-	if (hours < 0) {
-		hours = hours * -1;
-	} else if (hours == 00) {
-		hours = 12;
-	} else {
-		hours = hours;
-	}
-	
-	var currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
-	
-
-	if (alarmTime == currentTime) {
-		sound.play();
+		if (hours < 0) {
+			hours = hours * -1;
+		} else if (hours == 00) {
+			hours = 12;
+		} else {
+			hours = hours;
 		}
 
-},1000);
+		var currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+
+
+		if (alarmTime == currentTime) {
+			sound.play();
+		}
+
+	}, 1000);
 
 
 	// console.log('currentTime:' + currentTime);	

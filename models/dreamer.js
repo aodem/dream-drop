d1de558168f5dde1e0192e6521232a1134933b83
Edward Adam Farris <edwardadamfarris@Edwards-MacBook-Pro.local>
2019-03-02 12:27:34 -0700

    added cdn tags for bootdtrap and jquery, modified links in resource dropdown to redirect to a new page

--- models/dreamer.js
+++ models/dreamer.js
@@ -1,32 +1,32 @@
-// module.exports = function(sequelize, DataTypes) {
-//     var Dreamer = sequelize.define("Dreamer", {
-//       // Giving the dreamer model a name of type STRING
-//       name: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         },
-//         email: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//           },
-//           age: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false,
-//           },
-//           sex: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//             }
-//         });
+module.exports = function(sequelize, DataTypes) {
+    var Dreamer = sequelize.define("Dreamer", {
+      // Giving the dreamer model a name of type STRING
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+          sex: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            }
+        });
   
-//     Dreamer.associate = function(models) {
-//       // Associating dreamer with Posts
-//       // When an dreamer is deleted, also delete any associated Posts
-//       Dreamer.hasMany(models.Post, {
-//         onDelete: "cascade"
-//       });
-//     };
+    Dreamer.associate = function(models) {
+      // Associating dreamer with Posts
+      // When an dreamer is deleted, also delete any associated Posts
+      Dreamer.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
   
-//     return Dreamer;
-//   };
+    return Dreamer;
+  };
   

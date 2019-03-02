5367387b2c7f58dec956f8063b695819538a2222
Edward Adam Farris <edwardadamfarris@Edwards-MacBook-Pro.local>
2019-03-02 13:36:50 -0700

    dropped database and tables, modified and created new ones, changed some column names in handlebar bars to match db table names

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,22 @@ use dream_db;
 
 create table dreams (
     id int(10) NOT NULL AUTO_INCREMENT,
-    dream_description varchar NOT NULL,
+    dream_description varchar(255) NOT NULL,
     mood varchar(30) NOT NULL,
-    food_before_sleep varchar(30) NOT NULL,
+    food varchar(30) NOT NULL,
     hours_of_sleep int(2) NOT NULL,
+    category varchar(30) NOT NULL,
     tv_on boolean DEFAULT false,
     PRIMARY KEY (id)
 );
+
+create table dreamer (
+    id int(10) NOT NULL AUTO_INCREMENT,
+    dreamer_name varchar(40) NOT NULL,
+    age varchar(30) NOT NULL,
+    gender varchar(30) NOT NULL,
+    email varchar(30) NOT NULL,
+    tv_on boolean DEFAULT false,
+    PRIMARY KEY (id)
+);
+

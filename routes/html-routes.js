439112f99ace46b2a12dd2c93d4ad0ff4cd2cf44
debarge <43295961+aodem@users.noreply.github.com>
2019-03-01 18:14:46 -0700

    push 1.0

---
+++ routes/html-routes.js
@@ -0,0 +1,33 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  // cms route loads cms.html
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  // blog route loads blog.html
+  app.get("/dreamer", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/dreams.html"));
+  });
+
+  // authors route loads author-manager.html
+  app.get("/authors", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+  });
+};

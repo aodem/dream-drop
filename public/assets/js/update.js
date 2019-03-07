c26e27ca51055afe55d3994b39f81a853b478d8c
Beau <cummingsbeau@gmail.com>
2019-03-06 20:56:46 -0700

    updated update

--- public/assets/js/update.js
+++ public/assets/js/update.js
@@ -1,42 +1,50 @@
-function getPostData(id, type) {
-    var queryUrl;
-    switch (type) {
-        case "post":
-            queryUrl = "/api/dreamer/" + id;
-            break;
-        case "dreamer":
-            queryUrl = "/api/dream_descripton/" + id;
-            break;
-        default:
-            return;
+// Document.ready
+// call getPostData
+$(document).ready(function () {
+    function getPostData(id, type) {
+        var queryUrl;
+        switch (type) {
+            case "post":
+                queryUrl = "/api/dreamer/" + id;
+                break;
+            case "post":
+                queryUrl = "/api/dream_descripton/" + id;
+                break;
+            default:
+                return;
+        }
     }
+
+
+    getPostData();
     $.get(queryUrl, function (data) {
         if (data) {
             console.log(data.AuthorId || data.id);
-            // If this post exists, prefill our cms forms with its data
-            titleInput.val(data.title);
-            bodyInput.val(data.dream_descripton);
-            moodInput.val(data.mood);
-            foodInput.val(data.food);
-            hours_of_sleepInput.val(data.hours_of_sleep);
-            categoryInput.val(data.category);
-            tv_onInput.val(data.tv_on);
+            // If this post exists, prefill forms with its data
+            $("#title").text(data.title);
+            $("#body").text(data.dream_descripton);
+            $("mood").text(data.mood);
+            $("food").text(data.food);
+            $("hours_of_sleep").text(data.hours_of_sleep);
+            $("category").text(data.category);
+            $("tv_on").text(data.tv_on);
             dreamerId = data.DreamerId || data.id;
             // If we have a post with this id, set a flag for us to know to update the post
             // when we hit submit
             updating = true;
-            then(
-                function updatePost(post) {
-                    $.ajax({
-                      method: "PUT",
-                      url: "/api/posts",
-                      data: post
-                    })
-                      .then(function() {
-                        window.location.href = "/index";
-                      });
-                  }
-            )
         }
-    })
-};
+    });
+
+    $("updateDreamBtn").click(function (updatePost) {
+        console.log("dream updated");
+        $.ajax({
+            method: "PUT",
+            url: "/api/posts",
+            data: post
+        })
+            .then(function () {
+                window.location.href = "/";
+            });
+    }) 
+        
+})

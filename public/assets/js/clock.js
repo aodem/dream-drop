96d717251e1972134f8f3dd2b79e2ca846c74b3c
Beau <cummingsbeau@gmail.com>
2019-03-08 09:53:26 -0700

    added clock functionality

--- public/assets/js/clock.js
+++ public/assets/js/clock.js
@@ -1,159 +1,57 @@
-const sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav");
-sound.loop = true;
+const alarmButton = document.querySelector(".btn-alarm");
+const snoozeButton = document.querySelector(".btn-snooze");
+const stopButton = document.querySelector(".btn-stopalarm");
+const time = document.querySelector(".alarm-time");
+const alarmSound = new Audio();
 
-const h2 = document.getElementById('clock');
+alarmSound.src = ("/public/assets/js/clock.js");
+let alarmTimer;
+console.log(alarmButton);
+function setAlarm() {
+  let ms =
+    new Date().setHours(0, 0, 0, 0) +
+    time.valueAsNumber;
+  if (isNaN(ms)) {
+    alert("Cant be null pal");
+    return;
+  }
+  let alarm = new Date(ms);
+  var dt = new Date().getTime();
+  let differenceInMs = alarm.getTime() - dt;
 
-// display current time by the second
-const currentTime = setInterval(function () {
-	const date = new Date();
-
-	const hours = (12 - (date.getHours()));
-	// const hours = date.getHours();
-
-	const minutes = date.getMinutes();
-
-	const seconds = date.getSeconds();
-
-	const ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
-
-
-	//convert military time to standard time
-
-	if (hours < 0) {
-		hours = hours * -1;
-	} else if (hours == 00) {
-		hours = 12;
-	} else {
-		hours = hours;
-	}
-
-
-	h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
-
-}, 1000);
-
-
-/*functions to get hour, min, secs, 
-  am or pm, add zero, set alarm time and sound, clear alarm
-*/
-
-function addZero(time) {
-
-	return (time < 10) ? "0" + time : time;
-
+  if (differenceInMs < 0) {
+    alert(
+      "It looks like that's a date from the past!"
+    );
+    return;
+  }
+  alarmTimer = setTimeout(initAlarm, differenceInMs);
+  alarmButton.innerText = "Cancel Alarm";
+  alarmButton.setAttribute("onclick", "cancelAlarm(this);");
 }
 
-function hoursMenu() {
-
-	const select = document.getElementById('alarmhrs');
-	const hrs = 12
-
-	for (i = 1; i <= hrs; i++) {
-		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
-
-	}
-}
-hoursMenu();
-
-function minMenu() {
-
-	const select = document.getElementById('alarmmins');
-	const min = 59;
-
-	for (i = 0; i <= min; i++) {
-		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
-	}
-}
-minMenu();
-
-function secMenu() {
-
-	const select = document.getElementById('alarmsecs');
-	const sec = 59;
-
-	for (i = 0; i <= sec; i++) {
-		select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
-	}
-}
-secMenu();
-
-
-function alarmSet() {
-
-	const hr = document.getElementById('alarmhrs');
-
-	const min = document.getElementById('alarmmins');
-
-	const sec = document.getElementById('alarmsecs');
-
-	const ap = document.getElementById('ampm');
-
-
-	const selectedHour = hr.options[hr.selectedIndex].value;
-	const selectedMin = min.options[min.selectedIndex].value;
-	const selectedSec = sec.options[sec.selectedIndex].value;
-	const selectedAP = ap.options[ap.selectedIndex].value;
-
-	const alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
-	console.log('alarmTime:' + alarmTime);
-
-	document.getElementById('alarmhrs').disabled = true;
-	document.getElementById('alarmmins').disabled = true;
-	document.getElementById('alarmsecs').disabled = true;
-	document.getElementById('ampm').disabled = true;
-
-
-	//when alarmtime is equal to currenttime then play a sound
-	const h2 = document.getElementById('clock');
-
-	/*function to calcutate the current time 
-	then compare it to the alarmtime and play a sound when they are equal
-	*/
-
-	setInterval(function () {
-
-		const date = new Date();
-
-		const hours = (12 - (date.getHours()));
-		// const hours = date.getHours();
-
-		const minutes = date.getMinutes();
-
-		const seconds = date.getSeconds();
-
-		const ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
-
-
-		//convert military time to standard time
-
-		if (hours < 0) {
-			hours = hours * -1;
-		} else if (hours == 00) {
-			hours = 12;
-		} else {
-			hours = hours;
-		}
-
-		const currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
-
-
-		if (alarmTime == currentTime) {
-			sound.play();
-		}
-
-	}, 1000);
-
-
-	// console.log('currentTime:' + currentTime);	
-
+function cancelAlarm() {
+  clearTimeout(alarmTimer);
+  alarmButton.innerText = "Set Alarm";
+  alarmButton.setAttribute("onclick", "setAlarm(this);");
 }
 
-
-function alarmClear() {
-
-	document.getElementById('alarmhrs').disabled = false;
-	document.getElementById('alarmmins').disabled = false;
-	document.getElementById('alarmsecs').disabled = false;
-	document.getElementById('ampm').disabled = false;
-	sound.pause();
+function initAlarm() {
+  alarmSound.play();
+  alarmSound.loop = true;
+  options.style.display = "";
 }
+
+function stopAlarm() {
+  alarmSound.pause();
+  alarmSound.currentTime = 0;
+}
+
+function snooze() {
+  stopAlarm();
+  setTimeout(initAlarm, 5000);
+}
+
+alarmButton.addEventListener("click", setAlarm, false);
+snoozeButton.addEventListener("click", snooze, false);
+stopButton.addEventListener("click", stopAlarm, false);

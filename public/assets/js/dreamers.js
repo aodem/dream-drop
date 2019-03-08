ef1aad0ba1d75dab187610488a9c3f4511707704
debarge <43295961+aodem@users.noreply.github.com>
2019-03-07 19:24:03 -0700

    push 6.0

--- public/assets/js/dreamers.js
+++ public/assets/js/dreamers.js
@@ -1,47 +1,50 @@
 // demo page javascript 
 $(() => {
   //filter jquery
-  $().on("submit", e => {
-    event.preventDefault(e);
+  // $().on("submit", e => {
+  //   event.preventDefault(e);
 
-    // add chosen attribute. when that is changes to chosen grab the val and the name for the query string
+  //   // add chosen attribute. when that is changes to chosen grab the val and the name for the query string
 
-    let moodVal = $("select[name=]").val();
-    let categoryVal = $("select[name=]").val();
-    let preChoices = [moodVal, categoryVal];
-    let choices = [];
+  //   let moodVal = $("select[name=]").val();
+  //   let categoryVal = $("select[name=]").val();
+  //   let preChoices = [moodVal, categoryVal];
+  //   let choices = [];
 
-    preChoices.forEach(element => {
-      if (element !== " ") {
-        choices.push(element);
-      }
-    });
+  //   preChoices.forEach(element => {
+  //     if (element !== " ") {
+  //       choices.push(element);
+  //     }
+  //   });
 
-    let filterChoices = {
-      choices: choices
-    };
+  //   let filterChoices = {
+  //     choices: choices
+  //   };
 
-    $.ajax({
-      url: `/api/dreamers/filter/?dreamer=${writer},mood=${mood},category=${category}`,
-      method: "GET",
-      data: newDreamer
-    }).then(results => {
-      console.log(`results\n`);
-      console.log(
-        `New dreamer added:\n${newDreamer.name}\n${
-          newDreamer.email
-        }\n${newDreamer.age}\n${newDreamer.sex}\n`
-      );
-    });
-  });
+  //   $.ajax({
+  //     url: `/api/dreamers/filter/?dreamer=${writer},mood=${mood},category=${category}`,
+  //     method: "GET",
+  //     data: newDreamer
+  //   }).then(results => {
+  //     console.log(`results\n`);
+  //     console.log(
+  //       `New dreamer added:\n${newDreamer.name}\n${
+  //         newDreamer.email
+  //       }\n${newDreamer.age}\n${newDreamer.sex}\n`
+  //     );
+  //   });
+  // });
 
   // new dream submisssion
   $(".dream_create").on("submit", e => {
     event.preventDefault(e);
     console.log("hello");
+
+    console.log($("select[name=dreamer_name] option:selected").data("id"));
+
     
     let newDreamCreation = {
-      dream_title: $(),
+      dream_title: $("input[name=dream_title]").val(),
       dream_description: $("textarea[name=dream_description]").val().trim(),
       mood: $("input[name=mood]").val(),
       food: $("input[name=food]").val().trim(),
